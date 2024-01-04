2aad273f9b29ca53c4abc1bde0d6733689692944
Volkov Aleksandr <adamaandeva@gmail.com>
2024-01-04 11:46:47 +0300

    Немного сделал ClassDto и ClassDtoMapper в папке DTO (Возможно, нужно будет дополнить код)

--- RESTfull/RESTfull.API/DTO/ClassDto.cs
+++ RESTfull/RESTfull.API/DTO/ClassDto.cs
@@ -2,5 +2,11 @@
 {
     public class ClassDto
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public DateTime DateStart { get; set; }
+        public DateTime DateFinish { get; set; }
+        public int Room { get; set; }
+        public string Teacher { get; set; } = String.Empty;
     }
 }
